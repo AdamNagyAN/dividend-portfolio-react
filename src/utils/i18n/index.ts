7a02cf811f551c5afe90701b4897bda22223057d
Nagy Ádám <raween2000@gmail.com>
2022-12-18 21:10:26 +0100

    init translation

---
+++ src/utils/i18n/index.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+
+i18n
+	.use(Backend)
+	.use(initReactI18next)
+	.init({
+		fallbackLng: 'EN',
+		debug: false,
+		interpolation: {
+			escapeValue: false,
+		},
+		react: {
+			useSuspense: false,
+		},
+		backend: {
+			loadPath: () => {
+				const timestamp = new Date();
+				return `/locales/{{lng}}/{{ns}}.json?timestamp=${timestamp.getTime()}`;
+			},
+		},
+	});
+
+export default i18n;
