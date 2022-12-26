4d0ff20d078fdadd5646a173f04b80dc9f279379
Nagy Adam <raween2000@gmail.com>
2022-12-26 12:39:48 +0100

    Login page template

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from 'Routes';
 import TickerPage from './pages/ticker/TickerPage';
 import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/login/LoginPage';
 
 const AppRoutes: React.FC = () => {
 	return (
 		<Routes>
 			<Route path={ROUTES.HOME} element={<HomePage />} />
-			<Route path={ROUTES.LOGIN} element={<span>test</span>} />
+			<Route path={ROUTES.LOGIN} element={<LoginPage />} />
 			<Route path={ROUTES.TICKER(':symbol')} element={<TickerPage />} />
 		</Routes>
 	);
